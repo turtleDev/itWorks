6fc248239793fdf84d78525ed16d36d806a5518f
turtleDev <saravjeetamansingh@gmail.com>
2015-06-01 00:38:05 +0530

    Added sound to the notification pop up.

--- main.py
+++ main.py
@@ -5,27 +5,42 @@
 
 import subprocess
 import time
+import pygame.mixer
 
+pygame.mixer.init()
 
 cmd = "ping www.google.com -c 4"
 title = "Notification"
 message = "Your Internet is now working."
+sound_file = 'data/sound.ogg'
 
+def play_notification_sound(file_path):
+    sound = pygame.mixer.Sound(file_path)
+    channel = sound.play()
+    while channel.get_busy():
+        time.sleep(1)
 
-print "Working"
 
 def ping():
-    p = subprocess.call(cmd, shell=True, stdout=subprocess.PIPE)
+    p = subprocess.call(cmd,
+                        shell=True,
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.PIPE)
     # I've passed stdout=subprocess.PIPE to call() to supress any output.
     return p
-try:
-    while True:
-        result = ping()
-        if result == 0:
-            subprocess.call("notify-send '{}' '{}'".format(title, message), shell=True)
-            break
-        else:
-            time.sleep(4)
-except KeyboardInterrupt:
-    print "\b\bExiting"
 
+def main():
+    try:
+        while True:
+            result = ping()
+            if result == 0:
+                subprocess.call("notify-send '{}' '{}'".format(title, message), shell=True)
+                play_notification_sound(sound_file)
+                break
+            else:
+                time.sleep(4)
+    except KeyboardInterrupt:
+        print "\b\bExiting"
+
+if __name__ == '__main__':
+    main()
